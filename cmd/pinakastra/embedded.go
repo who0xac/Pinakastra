849b46e76e069db6e5ea48f2225fa31592a2d43e
who0xac <ayush.chauhanbs@gmail.com>
2025-12-13 13:24:14 +0530

    Move embedded.go to cmd/pinakastra with correct relative paths

--- cmd/pinakastra/embedded.go
+++ cmd/pinakastra/embedded.go
@@ -3,9 +3,9 @@ package main
 import "embed"
 
 // Embed web UI files
-//go:embed web/templates web/static
+//go:embed ../../web/templates ../../web/static
 var WebFiles embed.FS
 
 // Embed wordlists
-//go:embed wordlists
+//go:embed ../../wordlists
 var WordlistFiles embed.FS
